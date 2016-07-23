c9d1f332a41d380da197f68f9df723a2c7306bc7
tcarr863 <carrtyler98@gmail.com>
2016-07-23 12:38:05 -0500

    button and pic

--- js/script.js
+++ js/script.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
 $('img,header,footer').fadeIn(2000); 
 $('header').click(function(){ 
   $('header').css("background", "green");
-}); 
+  }); 
+  $('button').click(function(){ 
+  $('img').hide();
+});
 });
